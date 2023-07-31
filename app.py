d8508475d683db2706416fb382883105a7e257c8
Erikas-Ramanauskas <>
2023-07-31 22:29:38 +0000

    Initial commit

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 if __name__ == "__main__":
